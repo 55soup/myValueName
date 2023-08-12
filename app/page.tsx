a940539014cea6d47de00fea07cdcf8b708d7a60
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-08-12 14:17:36 +0900

    Add: menu sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 "use client"; // this is a client component 👈🏽
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSpeechRecognition } from "react-speech-kit";
 import styled from "styled-components";
-import { useCompletion } from 'ai/react'
+import { useCompletion } from "ai/react"
+import { BsMenuApp } from "react-icons/bs";
 
 export default function Home() {
   const [value, setValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false); // 햄버거 메뉴바 state
+
   const { listen, listening, stop } = useSpeechRecognition({
     onResult: (result: any) => {
       // 음성인식 결과가 value 상태값으로 할당됩니다.
       setValue(result);
     },
   });
+  const handleChangeValue = (e) => {
+    setValue(e.target.value);
+  }
+
   // toggle버튼 관리
   const [toggle, setToggle] = useState(false);
   const clickedToggle = () => {
@@ -29,56 +36,100 @@ export default function Home() {
     api: '/api'
   })
 
+  // db연동
+  const [faqs, setFaqs] = useState([]);
+
+  // useEffect(()=>{
+  //   const fetchFAQs = async () => {
+  //     const res = await fetch('./api/faqs.js');
+  //     const {faqsList} = await res.json();
+  //     setFaqs(faqsList);
+  //   }
+
+  //   fetchFAQs();
+  // })
+  /////////////////////////////////
   return (
     <Container>
-      <div>{listening && 'REC'}</div>
-      <RecordButton
-        onClick={() => {
-          clickedToggle();
-          !toggle ? listen({ interimResults: true }) : stop();
-        }}
-        toggle={toggle}
-      >
-        🎤
-      </RecordButton>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="변수명 추천을 받아보세요!"
-          style={{width: '25rem', fontSize: '2rem'}}
-          // className="w-full max-w-md border border-gray-300 rounded mb-8 shadow-xl p-2"
-          value={value}
-          onChange={handleInputChange}
-        />
-        <button type="submit">제출</button>
-      </form>
-      <Response>
-        <img src="/imgs/chatgpt.png" alt="chat gpt" style={{width: '5rem', height: '5rem'}}/>
-          <output style={{fontSize: '2rem'}}>{completion} 어떠세요?</output>
-          {/* <button disabled={isLoading} type="submit">
-            Send
-          </button> */}
-      </Response>
+      <LeftSidebar isOpen={isOpen}>
+        
+      </LeftSidebar>
+      <HamburgerBtn onClick={()=>{setIsOpen(isOpen ? false : true)}}><BsMenuApp size={30}/></HamburgerBtn>
+      <RightSidebar isOpen={isOpen}>
+        <div>{listening && 'REC'}</div>
+        <RecordButton
+          onClick={() => {
+            clickedToggle();
+            !toggle ? listen({ interimResults: true }) : stop();
+          }}
+          toggle={toggle}
+        >
+          🎤
+        </RecordButton>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="변수명 추천을 받아보세요!"
+            style={{width: '50rem', fontSize: '2rem', textAlign: 'center'}}
+            // className="w-full max-w-md border border-gray-300 rounded mb-8 shadow-xl p-2"
+            value={value}
+            onChange={handleChangeValue}
+          />
+          <button type="submit">제출</button>
+        </form>
+        <Response>
+          <img src="/imgs/chatgpt.png" alt="chat gpt" style={{width: '5rem', height: '5rem'}}/>
+            <output style={{fontSize: '2rem'}}>{completion} 어떠세요?</output>
+            {/* <button disabled={isLoading} type="submit">
+              Send
+            </button> */}
+        </Response>
+      </RightSidebar>
     </Container>
+    
   );
 }
 
 const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  background: white;
+`
+const RightSidebar = styled.div`
   display: flex;
+  width: ${props => props.isOpen ? '70%' : '50%' };
   height: 100vh;
   flex-direction: column;
   gap: 3rem;
   justify-content: center;
   align-items: center;
   background: white;
+  transition: ease  0.2s
 `;
-
+const HamburgerBtn = styled.button`
+  border: 0;
+  position: absolute;
+  left: 2rem;
+  top: 2rem;
+`
+const LeftSidebar = styled.div`
+  float: left;
+  width: 20%;
+  height: 100vh;
+  background: white;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  border-top-right-radius: 2rem;
+  border-bottom-right-radius: 2rem;
+  transform: translateX(${(props) => (props.isOpen ? '0' : '-50rem')});
+  transition: ease 0.2s;
+  // display: ${(props) => (props.isOpen ? 'block' : 'none')};
+`;
 const RecordButton = styled.button`
-  width: 4vw;
-  height: 4vw;
+  width: 5rem;
+  height: 5rem;
   background-color: ${(props) => (props.toggle ? "red" : "none")};
   border-radius: 5rem;
   border: 0;
-  transition: 0.3s;
+  transition: 0.2s;
   border: ${(props) => (props.toggle ? "none" : "solid 3px #d7d7d7")};
 `;
 const SpeechText = styled.div`
