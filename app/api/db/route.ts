c74fc7113fbb3cb46c5b9f89007fa2a0153601ac
55soup <w2118@e-mirim.hs.kr>
2023-08-24 09:02:40 +0900

    Refector: db connect 모듈화

--- app/api/db/route.ts
+++ app/api/db/route.ts
@@ -5,12 +5,19 @@ const oracledb = require('oracledb')
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT
 oracledb.autoCommit = true; // 자동 커밋
 
-export async function GET() {
-    let connection
+async function connectionDB(query:string, bindData:any = undefined) {
+    let connection, result;
     try {
         connection = await oracledb.getConnection(dbconfig);
-        const query = `SELECT * FROM GPT_QUESTIONS ORDER BY Q_ID`;
-        const result = await connection.execute(query);
+        if(bindData) // sql문에 넣어야할 데이터가 있다면
+            result = await connection.execute(query, bindData, (err:any, result:any) => {
+                if(err) console.error(err);
+                else return ;
+            });
+        else
+            result = await connection.execute(query);
+        
+        //return NextResponse.json(request.body);
         return NextResponse.json(result.rows);
     } catch (error) {
         console.log("dbtest error")
@@ -26,6 +33,12 @@ export async function GET() {
     }
 }
 
+export async function GET() {
+    const query = `SELECT * FROM GPT_QUESTIONS ORDER BY Q_ID`;
+    return connectionDB(query);
+
+}
+
 export async function POST(request: Request) {
     const bodyData = await request.json();
     console.log(bodyData);
@@ -37,27 +50,14 @@ export async function POST(request: Request) {
         bodyData.dates
     ];
 
-    let connection
-    try {
-        connection = await oracledb.getConnection(dbconfig); 
-        // const query = `INSERT INTO "SUNJO"."GPT_QUESTIONS" (USER_ID, CONTENT, ANSWER, DATES) VALUES (2, '햄버거 객체 이름 추천해줘', 'burgerObject, hamburgerObject, burgerOrder', 'Wed Aug 23 2023')`;
-        const query = `INSERT INTO "SUNJO"."GPT_QUESTIONS" (USER_ID, CONTENT, ANSWER, DATES) VALUES (:user_id, :content, :answer, :dates)`;
-        const result = await connection.execute(query, bindData, (err:any, result:any) => {
-            if(err) console.error(err);
-            else return ;
-        });
-        return NextResponse.json(request.body);
-        
-    } catch (error) {
-        console.log("dbtest error")
-        console.log(error)
-    } finally {
-        if (connection) {
-            try {
-                await connection.close()
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    }
+    const query = `INSERT INTO "SUNJO"."GPT_QUESTIONS" (USER_ID, CONTENT, ANSWER, DATES) VALUES (:user_id, :content, :answer, :dates)`
+    connectionDB(query, bindData);
+}
+
+export async function PATCH(request: Request){
+    
+}
+
+export async function DELETE(request: Request){
+    
 }
