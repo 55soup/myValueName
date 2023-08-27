cb560d270466ed28bc0f6ddca6d13cdce19cddc7
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-08-27 18:02:09 +0900

    #4 Fix: login formData -> body json, 로그인 로직

--- app/api/db/users/route.ts
+++ app/api/db/users/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse, NextRequest } from "next/server";
 import { redirect } from 'next/navigation'
 import connectionDB from "../dbConn";
+import dbconfig from "@/app/assets/dbconfig";
 
+const oracledb = require('oracledb')
 
 export async function GET() {
     const query = `SELECT * FROM users`;
@@ -10,13 +12,9 @@ export async function GET() {
 
 // login router
 export async function POST(request: NextRequest, response: NextResponse) {
-    // const bodyData = await request.json(); // body 내용 가져오기
-    // return NextResponse.json(bodyData);
-
-    /*
-    const formData = await request.formData(); // form 데이터에서 login input값 받아오기
-    const email = formData.get('email')
-    const password = formData.get('password')
+    let returnJSON;
+    const bodyData = await request.json(); // body 내용 가져오기
+    const [ email, password ] = [bodyData.email, bodyData.password];
 
     let connection
     try {
@@ -30,16 +28,30 @@ export async function POST(request: NextRequest, response: NextResponse) {
                 // db에서 가져온 데이터를 매칭함
                 const userInfo = result.rows[0];
                 if(userInfo !== undefined) {// 계정이 있다면
-                    // {status : 200, headers: {'Content-Type': 'application/json',}}
-                    if(password == userInfo.PASSWORD.trim()) console.log("로그인 성공") // 로그인 성공
-                    else console.log("로그인 실패"); // 로그인 실패
-                }else{
-                    console.log("계정이 존재하지 않습니다.");
+                    if(password == userInfo.PASSWORD.trim()) {
+                        // 로그인 성공
+                        returnJSON = {
+                            status : 200, 
+                            headers: {'Content-Type': 'application/json'},
+                            statusMessage : "login success"
+                        };  
+                    }else {
+                        // 로그인 실패
+                        returnJSON = {
+                            status : 401, 
+                            headers: {'Content-Type': 'application/json'},
+                            statusMessage: "invalid password"
+                        };  
+                    }
+                }else{ // 계정이 없다면
+                    returnJSON = {
+                        status : 401, 
+                        headers: {'Content-Type': 'application/json'},
+                        statusMessage: "invalid user id"
+                    };  
                 }
             }
         })
-        // 로그인 성공 후 홈으로 이동
-        // return NextResponse.json({});
     } catch (error) {
         console.log("dbtest error")
         console.log(error)
@@ -52,5 +64,6 @@ export async function POST(request: NextRequest, response: NextResponse) {
             }
         }
     }
-    */
+
+    return NextResponse.json(returnJSON);
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,51 @@
 "use client"
 import styled from "styled-components";
-import Router from "next/router";
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/navigation'
 
 export default function Login() {
+    const [email, setEmail] = useState<String>('');
+    const [password, setPassword] = useState<String>('');
+    const router = useRouter();
+
+    interface resMess {
+        status: number,
+        header: Object,
+        statusMessage: string
+    }
+
+    const toMain = (res:resMess) => {
+        console.log(res);
+        if(res.status === 200)
+            router.push('/');
+        else
+            alert(`로그인에 실패하였습니다. ${res.statusMessage}`);
+    }
+
+    const loginSubmit = () => {
+        fetch('/api/db/users', {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: JSON.stringify({ email : email, password : password }),
+          })
+          .then((res) => 
+            res.json()
+            .then((res) => 
+                toMain(res) // 상태코드가 200인 경우 메인페이지로           )
+          )
+    )}
+
+
     return(
-        <form action="/api/db/users" method="post">
-            <Container>
-                {/* <h1>various-variable : Chat GPT가 지어주는 변수명</h1> */}
-                <img src={"/imgs/logo-big.png"} width={200} />
-                <Input name="email" placeholder="이메일" required/>
-                <Input name="password" type="password" placeholder="비밀번호" required/>
-                <LoginBtn>로그인</LoginBtn>
-            </Container>
-        </form>
+        <Container>
+            {/* <h1>various-variable : Chat GPT가 지어주는 변수명</h1> */}
+            <img src={"/imgs/logo-big.png"} width={200} />
+            <Input name="email" placeholder="이메일" value={email} onChange={(e)=>{setEmail(e.target.value)}} required/>
+            <Input name="password" type="password" placeholder="비밀번호" value={password} onChange={(e)=>{setPassword(e.target.value)}} required/>
+            <LoginBtn onClick={loginSubmit}>로그인</LoginBtn>
+        </Container>
     );
 }
 
